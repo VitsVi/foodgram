7fb180a3d651f9796dca3726ae61813dbabf3b77
Vitsman <jigulsky.vitalik@yandex.ru>
2025-01-31 14:10:49 +0300

    datapush-4

--- backend/foodgram/core/management/commands/add_ingredients_data.py
+++ backend/foodgram/core/management/commands/add_ingredients_data.py
@@ -1,19 +1,21 @@
 import json
-
+from django.core.management.base import BaseCommand
 from recipe.models import Ingredient
 
-with open("data/ingredients.json", "r", encoding="utf-8") as file:
-    data = json.load(file)
+class Command(BaseCommand):
+    help = "Load ingredients data from JSON file"
 
-ingredients = [
-    Ingredient(
-        name=item["name"],
-        measurement_unit=item["measurement_unit"]
-    )
-    for item in data
-]
+    def handle(self, *args, **kwargs):
+        with open("data/ingredients.json", "r", encoding="utf-8") as file:
+            data = json.load(file)
 
+        ingredients = [
+            Ingredient(
+                name=item["name"],
+                measurement_unit=item["measurement_unit"]
+            )
+            for item in data
+        ]
 
-Ingredient.objects.bulk_create(ingredients)
-
-print("Загрузка завершена!")
+        Ingredient.objects.bulk_create(ingredients, ignore_conflicts=True)
+        self.stdout.write(self.style.SUCCESS("Ингредиенты успешно загружены."))
