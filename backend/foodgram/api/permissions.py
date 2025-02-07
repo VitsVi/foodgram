fab5de35101513882c30079f45c193c884c45ed8
Vitsman <jigulsky.vitalik@yandex.ru>
2025-02-07 11:57:49 +0300

    perm-fix

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -27,13 +27,12 @@ class RecipePermission(BasePermission):
         """Проверка доступа на уровне запроса."""
         return (
             request.method == "GET"
-            or request.user and request.user.is_authenticated
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         """Проверка доступа на уровне объекта (рецепта)."""
         return (
-            (request.method in ['PATCH', 'PUT']
-            and obj.author == request.user)
-            or request.method in SAFE_METHODS
+            request.method in SAFE_METHODS
+            or obj.author == request.user
         )
